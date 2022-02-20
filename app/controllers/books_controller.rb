41f45f4c9b772ed6c46738398b0858ff4f669b3f
kn1025yu <sn22kzh@gmail.com>
2022-02-20 16:23:06 +0000

    [add]投稿機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,30 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
     @books = Book.all
   end
-  
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to index
+  end
+
   def show
   end
-  
+
   def edit
   end
-  
+
   def updated
   end
-  
+
   def destroy
-    
+
+  end
+
+private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
