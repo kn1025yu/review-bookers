5360d71219114594fcbabedb0b1e7c5fe0ee3708
kn1025yu <sn22kzh@gmail.com>
2022-02-28 16:04:42 +0000

    [fix]editページの編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,7 +18,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  def updated
+  def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book)
